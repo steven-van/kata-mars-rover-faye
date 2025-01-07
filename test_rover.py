d99c5017a64ad45d9280672c7c886b189148d0b5
Fenyx <64859587+steven-van@users.noreply.github.com>
2025-01-07 16:55:49 +0100

    fix: moveBackward, turnRight, turnLeft

--- test_rover.py
+++ test_rover.py
@@ -1,7 +1,8 @@
 class Rover:
-    def __init__(self, x, y, direction):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.direction = 'NORTH'
 
     def executeCommands(self, commands):
         for command in commands:
@@ -19,9 +20,9 @@ class Rover:
     def moveBackward(self):
         self.y -= 1
     def turnRight(self):
-        self.x += 1
+        self.direction = 'EAST'
     def turnLeft(self):
-        self.x -= 1
+        self.direction = 'WEST'
 
 def test_rover_moves_forward():
     # Given : The rover is at position (0,0) facing North
@@ -44,6 +45,7 @@ def test_rover_moves_backward():
     # Then : The rover moves to position (0,-1)
     assert rover.x == 0, f"Expected x=0, got {rover.x}"
     assert rover.y == -1, f"Expected y=-1, got {rover.y}"
+    assert rover.direction == 'NORTH'
     
 def test_rover_turns_left():
     # Given : The rover is at position (0,0) facing North
@@ -51,10 +53,7 @@ def test_rover_turns_left():
     
     # When : The rover receives the command l
     rover.executeCommands(['l'])
-    
-    # Then : The rover moves to position (-1,0)
-    assert rover.x == -1, f"Expected x=-1, got {rover.x}"
-    assert rover.y == 0, f"Expected y=0, got {rover.y}"
+    assert rover.direction == 'WEST'
 
 def test_rover_turns_right():
     # Given : The rover is at position (0,0) facing North
@@ -63,6 +62,31 @@ def test_rover_turns_right():
     # When : The rover receives the command r
     rover.executeCommands(['r'])
     
-    # Then : The rover moves to position (1,0)
-    assert rover.x == 1, f"Expected x=-1, got {rover.x}"
+    assert rover.direction == 'EAST'
+
+def test_rover_turns_left_and_forward():
+    # Given : The rover is at position (0,0) facing North
+    rover = Rover(0, 0)
+    
+    # When : The rover receives the command l, f
+    rover.executeCommands(['l'])
+    rover.executeCommands(['f'])
+
+    # Then : The rover moves to position (-1,0)
+    assert rover.x == -1, f"Expected x=-1, got {rover.x}"
     assert rover.y == 0, f"Expected y=0, got {rover.y}"
+    assert rover.direction == 'WEST'
+
+
+def test_rover_turns_right_and_forward():
+    # Given : The rover is at position (0,0) facing North
+    rover = Rover(0, 0)
+    
+    # When : The rover receives the command r, f
+    rover.executeCommands(['r'])
+    rover.executeCommands(['f'])
+
+    # Then : The rover moves to position (1,0)
+    assert rover.x == 1, f"Expected x=1, got {rover.x}"
+    assert rover.y == 0, f"Expected y=0, got {rover.y}"
+    assert rover.direction == 'EAST'
